3988dc37b3a7de393c2baa8148db864d5675f23c
Alejandro Soto <64869366+Achellx@users.noreply.github.com>
2026-06-29 21:51:20 -0600

    fix: rename the gender column to genre

--- database/script.sql
+++ database/script.sql
@@ -13,7 +13,7 @@ CREATE TABLE Movies (
     id INT AUTO_INCREMENT PRIMARY KEY,
     Name VARCHAR(100) NOT NULL,
     ReleaseYear DATE NOT NULL,
-    Gender VARCHAR(50) NOT NULL,
+    Genre VARCHAR(50) NOT NULL,
     Duration TIME NOT NULL,
     FKDirector INT NOT NULL,
     FOREIGN KEY (FKDirector) REFERENCES Director(id)
